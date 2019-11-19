85d9529566783e049f865a2257f420cf006e028b
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-19 21:41:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,15 @@
 class Dog 
-  def initalize(name)
+  def initalize(name, breed)
     @name = name
+    @breed = breed
   end
   def name=(new_name)
     @name = new_name 
   end 
   def name 
     @name 
+  end
+  def breed=(new_breed)
+    @bread = new_breed
   end
 end 
